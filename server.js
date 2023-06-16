9ccb1282325f396bce680f9b7836e98c1011b0a5
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2023-06-16 18:53:32 +0500

    login/register pages

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Welcome to api!",
+  });
+});
+
+app.post("/api/posts", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: "Post created...",
+        authData,
+      });
+    }
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  const users = {
+    id: 1,
+    username: "jasur",
+    email: "jasur@gmail.com",
+  };
+
+  jwt.sign({ users }, "secretKey", { expiresIn: "1h" }, (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+app.listen(5001, () => console.log("Server is running on port 5000"));
